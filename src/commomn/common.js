e231a9b86c86a17e0bb9b938b8a62f80473be16b
taichi-arai-jointcrew <taichi.arai@jointcrew.co.jp>
2018-03-29 15:54:34 +0900

    モックテスト用

---
+++ src/commomn/common.js
@@ -0,0 +1,24 @@
+import {StyleSheet} from 'react-native'
+
+const CommonStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    backgroundColor: '#F5FCFF',
+    alignContent:'flex-start',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+export default CommonStyles;
